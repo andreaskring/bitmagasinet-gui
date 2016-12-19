c8f36d87873a332d6d9cce480e61afacf7951479
Andreas Kring <andreas@magenta.dk>
2016-12-19 18:18:08 +0100

    Updated POM to build a jar with dependencies

--- src/main/java/dk/magenta/bitmagasinet/remote/BitrepositoryConnector.java
+++ src/main/java/dk/magenta/bitmagasinet/remote/BitrepositoryConnector.java
@@ -1,10 +1,12 @@
 package dk.magenta.bitmagasinet.remote;
 
+import javax.jms.JMSException;
+
 import dk.magenta.bitmagasinet.checksum.FileChecksum;
 
 public interface BitrepositoryConnector {
 
 	public String getRemoteChecksum(FileChecksum fileChecksum) throws Exception;
-	public void closeMessageBus();
+	public void closeMessageBus() throws JMSException;
 	
 }
